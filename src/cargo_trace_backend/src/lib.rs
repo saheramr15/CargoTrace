d81827e476fc34824d9aa81f8e9e57573bfd6e08
saher amr <saher2110197@miuegypt.edu.eg>
2025-07-24 04:42:09 +0300

    .

--- src/cargo_trace_backend/src/lib.rs
+++ src/cargo_trace_backend/src/lib.rs
@@ -1,15 +1,41 @@
 use ic_cdk::query;
 use ic_cdk::update;
+use ic_stable_structures::{
+    memory_manager::{MemoryId, MemoryManager, VirtualMemory},
+    DefaultMemoryImpl, StableBTreeMap,
+};
+use std::cell::RefCell;
 
-#[query]
-fn hello(name: String) -> String {
-    format!("Hello, {}!", name)
+type Memory = VirtualMemory<DefaultMemoryImpl>;
+
+thread_local! {
+    static MEMORY_MANAGER: RefCell<MemoryManager<DefaultMemoryImpl>> =
+        RefCell::new(MemoryManager::init(DefaultMemoryImpl::default()));
+
+    static ID_SET: RefCell<StableBTreeMap<u128, u8, Memory>> = RefCell::new(
+        StableBTreeMap::init(
+            MEMORY_MANAGER.with(|m| m.borrow().get(MemoryId::new(0)))
+        )
+    );
 }
 
 #[update]
-fn world(name: String) -> String {
-    format!("World, {}!", name)
+fn add_id(id: u128) -> bool {
+    ID_SET.with(|set| set.borrow_mut().insert(id, 1).is_none())
 }
 
-// Enable Candid export
+#[query]
+fn has_id(id: u128) -> bool {
+    ID_SET.with(|set| set.borrow().contains_key(&id))
+}
+
+#[update]
+fn remove_id(id: u128) -> bool {
+    ID_SET.with(|set| set.borrow_mut().remove(&id).is_some())
+}
+
+#[query]
+fn get_all_ids() -> Vec<u128> {
+    ID_SET.with(|set| set.borrow().iter().map(|entry| *entry.key()).collect())
+}
 ic_cdk::export_candid!();
