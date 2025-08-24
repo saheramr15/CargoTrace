e8c3867a83b5da2327624a39fe6a394de332ff20
saher amr <saher2110197@miuegypt.edu.eg>
2025-08-24 04:24:56 +0300

    Update main.jsx

--- src/cargo_trace_frontend/src/main.jsx
+++ src/cargo_trace_frontend/src/main.jsx
@@ -3,9 +3,15 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import './styles/main.css';
+import { AuthProvider } from "./context/AuthContext";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+       <AuthProvider>
+
+          <App />
+      </AuthProvider>
+
   </React.StrictMode>,
 );
