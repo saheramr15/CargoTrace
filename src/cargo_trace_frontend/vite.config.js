f2600c784c07c7c29dbb0b6b8cc4370a67f37a05
Mohamed Ali <mohamed.ali300094@gmail.com>
2025-09-19 06:03:59 +0300

    add tailwind

--- src/cargo_trace_frontend/vite.config.js
+++ src/cargo_trace_frontend/vite.config.js
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'url';
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import environment from 'vite-plugin-environment';
+import tailwindcss from '@tailwindcss/vite';
 import dotenv from 'dotenv';
 dotenv.config({ path: '../../.env' });
 
@@ -26,6 +27,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    tailwindcss(),
     environment("all", { prefix: "CANISTER_" }),
     environment("all", { prefix: "DFX_" }),
   ],
